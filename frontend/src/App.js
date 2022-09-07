c0c8d3b1a5ac42fed31757eb8bbe90719d5fddec
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2022-09-08 02:30:23 +0300

    first api call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Header from './components/Header';
 import Search from './components/search';
 
 
-  
+const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState('');
@@ -14,8 +14,21 @@ function App() {
   const submithandler= function submithandler(e) {
     e.preventDefault() ;
     console.log(word) ;
+
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        })
+
+        .catch((err) => {
+          console.log(err);
+        })
   }
   //console.log(word);
+
+  //console.log(process.env.REACT_APP_UNSPLASH_KEY);
+  // THIS LINE MUST BE DELETED
 
   return (
     <div >
