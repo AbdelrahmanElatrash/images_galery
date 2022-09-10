657987a4eb010c4bc8c223c6a5af7894d4ce1b55
AbdelrahmanElatrash <a.elatrash170@gmail.com>
2022-09-10 13:12:45 +0300

    eslint and prettier setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,40 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Header from "./components/Header";
+import Search from "./components/search";
 
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
-import Header from './components/Header';
-import Search from './components/search';
-
-
-const UNSPLASH_KEY=process.env.REACT_APP_UNSPLASH_KEY;
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
-  const submithandler= function submithandler(e) {
-    e.preventDefault() ;
-    console.log(word) ;
+  const submithandler = function submithandler(e) {
+    e.preventDefault();
+    console.log(word);
 
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-        })
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
 
-        .catch((err) => {
-          console.log(err);
-        })
+      .catch((err) => {
+        console.log(err);
+      });
 
-        setWord('');   // to cear search input and back it to default
-  }
+    setWord(""); // to clear search input and back it to default
+  };
   //console.log(word);
 
   //console.log(process.env.REACT_APP_UNSPLASH_KEY);
   // THIS LINE MUST BE DELETED
 
   return (
-    <div >
-      <Header title='Images Gallery' />
-      <Search word={word} setWord={setWord} submithandler={submithandler}/>
+    <div>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} submithandler={submithandler} />
     </div>
   );
 }
